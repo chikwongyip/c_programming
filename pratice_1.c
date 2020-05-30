4dedf3b769144204c33682b5131cc33c692853f4
chikwongyip <527439024@qq.com>
2020-05-30 21:52:08 +0800

    chapter one

---
+++ pratice_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char name[10];
+    char address[80];
+    
+    printf("Kane Yip \n"); //第一次打印内容
+    printf("Kane \n Yip\n");
+    printf("Kane ");
+    printf("Yip");
+    //printf("Name:%d\n,address:%d\n",'Kane Yip','badasd2esdsa');
+}
